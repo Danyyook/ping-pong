c363d110168d43d72c4da9f7ad1260cf61a6be8f
Danyyook <162796597+Danyyook@users.noreply.github.com>
2024-03-30 13:53:05 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -13,16 +13,40 @@ class GameSprite(sprite.Sprite):
         self.speed = player_speed
         self.rect.x = player_x
         self.rect.y = player_y
-
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y <440:
+            self.rect.y += self.speed  
+class Player2(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y <440:
+            self.rect.y += self.speed 
+
+
+
+levish = Player('palka.png', 5, 200, 3, 20, 160)
+pravish = Player2('palka.png', 475, 200, 3, 20, 160) 
 clock = time.Clock()
 game = True
 while game:
+    window.fill(back)
     for e in event.get():
         if e.type == QUIT:
             game = False
+    levish.update_l()
+    pravish.update_r()
 
+    levish.reset()
+
+    pravish.reset()
     display.update()
-    clock.tick(60)
+    clock.tick(180)
