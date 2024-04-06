5650f04a6e6190a7e1dea22f62aff64f554df73f
Danyyook <162796597+Danyyook@users.noreply.github.com>
2024-04-06 13:32:24 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -29,24 +29,47 @@ class Player2(GameSprite):
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y <440:
-            self.rect.y += self.speed 
+            self.rect.y += self.speed
+speed_x = 3
+speed_y = 3
 
 
 
 levish = Player('palka.png', 5, 200, 3, 20, 160)
 pravish = Player2('palka.png', 475, 200, 3, 20, 160) 
+ball = GameSprite('kake.png', 120,120,3,50,50)
 clock = time.Clock()
+font.init()
+font1 = font.Font(None, 50)
+lose1 = font1.render('PLayer 1 lose', True, (0,0,0))
+font2 = font.Font(None, 50)
+lose2 = font2.render('PLayer 2 lose', True, (0,0,0))
 game = True
+finish = False
 while game:
-    window.fill(back)
     for e in event.get():
         if e.type == QUIT:
             game = False
-    levish.update_l()
-    pravish.update_r()
-
-    levish.reset()
-
-    pravish.reset()
+    if not finish:
+        window.fill(back)
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(levish, ball) or sprite.collide_rect(pravish, ball):
+            speed_x *= -1
+            speed_y*= 1
+        if ball.rect.y > win_hieght - 50 or ball.rect.y < 0:
+            speed_y *= -1
+            speed_x *= 1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200,200))
+        if ball.rect.x > win_hieght-50:
+            finish = True
+            window.blit(lose2, (200,200))
+        levish.update_l()
+        pravish.update_r()
+        levish.reset()
+        pravish.reset()
+        ball.reset()
     display.update()
-    clock.tick(180)
+    clock.tick(60)
